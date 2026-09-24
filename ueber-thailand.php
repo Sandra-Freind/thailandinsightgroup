651d1c9ab7668ae808497f9cb89850e62c399bc4
Sandra-Freind <mail@ralfpleines.de>
2026-09-24 19:05:47 +0200

    Remove introductory sentence from Über Thailand

--- ueber-thailand.php
+++ ueber-thailand.php
@@ -4,5 +4,6 @@ $pageTitle = 'Reiseziele';
 $pageDescription = 'Reiseziele in Thailand – Regionen, Geografie und Reiseinformationen.';
 require __DIR__ . '/includes/page-start.php';
 $currentGuide = 'geografie';
+$hideTravelIntro = true;
 require __DIR__ . '/includes/travel-body.php';
 require __DIR__ . '/includes/page-end.php';
