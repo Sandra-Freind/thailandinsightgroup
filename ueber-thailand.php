310664434819c8104c67c652d6a65becc22c30c8
Sandra-Freind <mail@ralfpleines.de>
2026-09-25 11:33:18 +0200

    Use full green mountain panorama on Thailand hero

--- ueber-thailand.php
+++ ueber-thailand.php
@@ -5,8 +5,8 @@ $pageDescription = 'Reiseziele in Thailand – Regionen, Geografie und Reiseinfo
 require __DIR__ . '/includes/page-start.php';
 $currentGuide = 'geografie';
 $hideTravelIntro = true;
-$travelHeroImage = 'assets/img/ueber-thailand-natur-khao-sok.jpg';
-$travelHeroAlt = 'Grüne Waldlandschaft am Cheow-Lan-See im Khao-Sok-Nationalpark';
+$travelHeroImage = 'assets/img/ueber-thailand-panorama-mae-fa-luang.jpg';
+$travelHeroAlt = 'Grüne Berglandschaft bei Mae Fa Luang in Nordthailand';
 $travelHeroCredit = true;
 require __DIR__ . '/includes/travel-body.php';
 require __DIR__ . '/includes/page-end.php';
