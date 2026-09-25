be834f65237e16c8dfb638f9d68e40c591002d66
Sandra-Freind <mail@ralfpleines.de>
2026-09-25 12:01:31 +0200

    Replace Thailand hero with vivid Chiang Mai rice terraces panorama

--- ueber-thailand.php
+++ ueber-thailand.php
@@ -5,8 +5,8 @@ $pageDescription = 'Reiseziele in Thailand – Regionen, Geografie und Reiseinfo
 require __DIR__ . '/includes/page-start.php';
 $currentGuide = 'geografie';
 $hideTravelIntro = true;
-$travelHeroImage = 'assets/img/ueber-thailand-pai-weite.jpg';
-$travelHeroAlt = 'Grüne Reisfelder und Berglandschaft bei Pai in Nordthailand';
+$travelHeroImage = 'assets/img/ueber-thailand-chiangmai-reisterrassen.jpg';
+$travelHeroAlt = 'Leuchtend grüne Reisterrassen und weiter Blick auf die Berge bei Chiang Mai, Thailand';
 $travelHeroCredit = false;
 require __DIR__ . '/includes/travel-body.php';
 require __DIR__ . '/includes/page-end.php';
